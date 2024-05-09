6415ced4c3932dbb4ab2ea05230f5587f88bb2e2
skiii08 <146687825+skiii08@users.noreply.github.com>
2024-05-09 20:41:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
     topImage.addEventListener('click', function() {
         initialScreen.style.display = 'none';
         mapContainer.style.display = 'block';
-        map.src = "エリアマップ\全体.jpg"; // ここで全体マップの画像のパスを指定
+        map.src = "エリアマップ/全体.jpg"; // ここで全体マップの画像のパスを指定
     });
 
     map.addEventListener('click', function(e) {
